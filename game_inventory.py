46d61b21c795eff2e20bf2043397415aa58814c3
lolekgk <karol.gajda97@gmail.com>
2022-02-22 14:35:26 +0100

    Add error exceptions in import and export_inventory

--- game_inventory.py
+++ game_inventory.py
@@ -35,23 +35,27 @@ def print_table(inventory, order=None):
 
 def import_inventory(inventory, filename = 'import_inventory.csv'):
     """Import new inventory items from a CSV file."""
-
-    with open(filename, "r") as file:
-        items_list = file.readline().split(",")
-    for element in items_list:
-        inventory.setdefault(element, items_list.count(element))
-    return inventory
+    try:
+        with open(filename, "r") as file:
+            items_list = file.readline().split(",")
+        for element in items_list:
+            inventory.setdefault(element, items_list.count(element))
+        return inventory
+    except FileNotFoundError:
+        print(f"File '{filename}' not found!")
 
 
 def export_inventory(inventory, filename = 'export_inventory.csv'):
     """Export the inventory into a CSV file."""
-
-    items_list = list(inventory.keys())
-    with open(filename, "w") as file:
-        for item in items_list:
-            if item != items_list[-1]:
-                item = ((item + '\t') * inventory[item]).replace('\t', ',')
-            file.write(item)
+    try:
+        items_list = list(inventory.keys())
+        with open(filename, "w") as file:
+            for item in items_list:
+                if item != items_list[-1]:
+                    item = ((item + '\t') * inventory[item]).replace('\t', ',')
+                file.write(item)
+    except OSError:
+        print(f"You don't have permission creating file '{filename}'!")
 
 
 if __name__ == "__main__":
