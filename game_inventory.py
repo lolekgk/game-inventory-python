b62eafc5e3eed9e5243ab5a49dbb375740386390
lolekgk <karol.gajda97@gmail.com>
2022-02-22 15:34:18 +0100

    Add function add_to_inventory
    Add_to_inventory works as assumed. Main() only for testing.

--- game_inventory.py
+++ game_inventory.py
@@ -5,22 +5,31 @@
 
 
 def main():
-    inventory = {}
-    imported_inventory = import_inventory(inventory, 'test_inventory.csv')
-    display_inventory(imported_inventory)
+    # inventory = {}
+    # import_inventory(inventory, 'test_inventory.csv')
+    # display_inventory(inventory)
+    # loot = ['sword', 'ork tooth', 'fms', 'fms', 'fms']
+    inventory = {'rope': 1, 'torch': 6}
+    added_items = ['torch', 'torch', 'torch', 'fms', 'fms']
+    add_to_inventory(inventory, added_items)
 
 
 def display_inventory(inventory):
     """Display the contents of the inventory in a simple way."""
-    print("Inventory")
-    for k, v in inventory.items():
-        print(k + ':', v)
+    if inventory:
+        for key, value in inventory.items():
+            print(key + ':', value)
     
 
 def add_to_inventory(inventory, added_items):
     """Add to the inventory dictionary a list of items from added_items."""
-    pass
-
+    for item in added_items:
+        if item in inventory:
+            new_value = inventory[item] + 1
+            inventory.update({item : new_value})  
+        inventory.setdefault(item, added_items.count(item))
+    return inventory
+    
 
 def remove_from_inventory(inventory, removed_items):
     """Remove from the inventory dictionary a list of items from removed_items."""
